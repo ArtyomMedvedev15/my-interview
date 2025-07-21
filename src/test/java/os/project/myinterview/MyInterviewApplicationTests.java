fa4b11fb9c2e29cd628485707cc93d639ef14512
a.miadzvedzeu <arthaos14@gmail.com>
2025-07-21 22:54:19 +0300

    init commit

---
+++ src/test/java/os/project/myinterview/MyInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package os.project.myinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
